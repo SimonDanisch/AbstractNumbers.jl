cfb623a9e2ec44af5cd4a78209ad3780d280116d
SimonDanisch <sdanisch@gmail.com>
2017-11-22 11:29:49 +0100

    add convert functions

--- src/AbstractNumbers.jl
+++ src/AbstractNumbers.jl
@@ -8,12 +8,29 @@ using SpecialFunctions
 abstract type AbstractNumber{T} <: Number end
 
 # convert(Number, my_type) & is the only interface one needs to overload
-@inline basetype(T::Type{<: AbstractNumber}) = error("base type not implemented for $T")
+@inline basetype(T::Type{<: AbstractNumber}) = error("AbstractNumbers.basetype not implemented for $T")
 @inline basetype(x::T) where T <: AbstractNumber = basetype(T)
 @inline number(x::AbstractNumber) = convert(Number, x)
+@inline function Base.convert(::Type{AN}, x::AbstractNumber{T2}) where {AN <: AbstractNumber{T}, T2} where T
+    AN(T(convert(Number, x)))
+end
+@inline function Base.convert(::Type{AN}, x::Number) where AN <: AbstractNumber
+    AN(x)
+end
+@inline function Base.convert(::Type{T}, x::AbstractNumber) where T <: Number
+    T(convert(Number, x))
+end
 
-
-@inline like(::AN, x::T) where {AN <: AbstractNumber, T} = like(AN, x)
+"""
+    like(num::AbstractNumber, x::T)
+Creates a number from `x` like the first argument. It discards the eltype of `num`
+and uses the type of `x` instead.
+usage:
+    ```
+    like(MyAbstractNumber(1f0), 22) === MyAbstractNumber{Int}(22)
+    ```
+"""
+@inline like(num::AN, x::T) where {AN <: AbstractNumber, T} = like(AN, x)
 @inline like(::Type{AN}, x::T) where {AN <: AbstractNumber, T} = basetype(AN){T}(x)
 # Not sure if this is the most performant and correct way, but this enables
 # that == and friends return a bool. Returning an AbstractNumber{Bool} seems a bit odd
@@ -27,5 +44,6 @@ include("overloads.jl")
 
 rem(x::AbstractNumber, y::AbstractNumber, r::RoundingMode) = like(x, rem(number(x), number(y), r))
 
+export AbstractNumber
 
 end # module
