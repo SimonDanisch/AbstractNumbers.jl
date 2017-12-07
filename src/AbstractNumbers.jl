bd4d683d72fa6b252eec5e93f890a1847724f2f3
SimonDanisch <sdanisch@gmail.com>
2017-12-07 23:17:28 +0100

    fix stack overflow

--- src/AbstractNumbers.jl
+++ src/AbstractNumbers.jl
@@ -10,7 +10,7 @@ abstract type AbstractNumber{T} <: Number end
 # convert(Number, my_type) & is the only interface one needs to overload
 @inline basetype(T::Type{<: AbstractNumber}) = error("AbstractNumbers.basetype not implemented for $T")
 @inline basetype(x::T) where T <: AbstractNumber = basetype(T)
-@inline number(x::AbstractNumber) = convert(Number, x)
+@inline number(x::AbstractNumber) = error("AbstractNumbers.number not implemented for $T")
 @inline function Base.convert(::Type{AN}, x::AbstractNumber{T2}) where {AN <: AbstractNumber{T}, T2} where T
     AN(T(convert(Number, x)))
 end
@@ -18,7 +18,7 @@ end
     AN(x)
 end
 @inline function Base.convert(::Type{T}, x::AbstractNumber) where T <: Number
-    T(convert(Number, x))
+    T(number(x))
 end
 
 """
